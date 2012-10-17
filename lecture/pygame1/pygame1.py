f2b4a62f9f7a9a8079ef40f65588b96fdcc2605e
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2012-10-17 12:59:21 -0400

    In-class updates

--- lecture/pygame1/pygame1.py
+++ lecture/pygame1/pygame1.py
@@ -3,40 +3,47 @@
 
 import pygame
 
-pygame.init()   # Initialized pygame module
-
-# Screen size
 width, height = 640, 480
 
-screen = pygame.display.set_mode((width, height))
+def init():
+    pygame.init()   # Initialized pygame module
 
-ball = pygame.image.load("ball.gif").convert_alpha()
-pygame.draw.circle(ball, pygame.Color('magenta'), (25, 25), 10)
-ball_rect = ball.get_rect()
+    # Screen
+    return pygame.display.set_mode((width, height))
 
-horizontal = 1
-vertical = 3
+def main(screen):
+    # Load the image
+    ball = pygame.image.load("ball.gif").convert_alpha()
+    pygame.draw.circle(ball, pygame.Color('green'), (25, 25), 10)
+    ball_rect = ball.get_rect()
 
-running = True
-while running:
-#    print 'looping'
+    horizontal = 1
+    vertical = 2
 
-    screen.fill((0, 0, 0))          # Redraw background
-    screen.blit(ball, ball_rect)    # Draw ball to screen
-    pygame.display.flip()           # Display screen in window
-    ball_rect[0] += horizontal      # Move ball, 1 pixel right
-    ball_rect[1] += vertical        # Move ball, 1 pixel right
+    running = True
 
-    if ball_rect.right >= width:
-        horizontal = -1
-    elif ball_rect.left <= 0:
-        horizontal = 1
-    if ball_rect.bottom >= height:
-        vertical = -3
-    elif ball_rect.top <= 0:
-        vertical = 3
+    while running:
+    #    print 'looping'
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
-            # exit()
-            running = False
+        screen.fill((0, 0, 0))          # Redraw background
+        screen.blit(ball, ball_rect)    # Draw ball to screen
+        pygame.display.flip()           # Display screen in window
+
+        # Ball motion
+        # ball_rect = ball_rect.move(horizontal, vertical)      Poor version, that makes new rectangles
+        ball_rect.move_ip(horizontal, vertical)
+
+        # Bounds checking
+        if ball_rect.right >= width or ball_rect.left <= 0:
+            horizontal *= -1
+        if ball_rect.bottom >= height or ball_rect.top <= 0:
+            vertical *= -1
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                # exit()
+                running = False
+
+
+screen = init()
+main(screen)
